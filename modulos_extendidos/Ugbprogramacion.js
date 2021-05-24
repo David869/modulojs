2d9b71bdc44a8399c384442beb7fa6f2eaef3098
david <david1098cr@hotmail.com>
2021-05-23 20:16:33 -0600

    version 2 creando modulo extendido

---
+++ modulos_extendidos/Ugbprogramacion.js
@@ -0,0 +1,12 @@
+var sm=require('./programacion.js');
+exports.Ugbprogramacion=function()
+
+{
+console.log("Bienvenido a programacionIV")
+
+function ugb()
+{
+    var ugb=sm
+    ugb.programacion;
+}
+}
